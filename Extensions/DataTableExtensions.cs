62685c95a92f42def90479af67d32bb9b15abc63
Jorge Marcos Kawas <jorgemk85@hotmail.com>
2018-08-08 17:38:42 -0500

    Se agregaron extensiones para los tipos List y DataTable que pretenden incrementar la usabilidad de la libreria.

---
+++ Extensions/DataTableExtensions.cs
@@ -0,0 +1,35 @@
+﻿using DataManagement.Tools;
+using System.Collections.Generic;
+using System.Data;
+
+namespace DataManagement.Extensions
+{
+    public static class DataTableExtensions
+    {
+        public static T ToObject<T>(this DataTable dataTable) where T : new()
+        {
+            return DataSerializer.ConvertDataTableToObjectOfType<T>(dataTable);
+        }
+
+        public static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(this DataTable dataTable, string keyName, string valueName)
+        {
+            return DataSerializer.ConvertDataTableToDictionary<TKey, TValue>(dataTable, keyName, valueName);
+        }
+
+        public static List<T> ToList<T>(this DataTable dataTable) where T : new()
+        {
+            return DataSerializer.ConvertDataTableToListOfType<T>(dataTable);
+        }
+
+        public static string ToJson<T>(this DataTable dataTable) where T : new()
+        {
+            return DataSerializer.SerializeDataTableToJsonListOfType<T>(dataTable);
+        }
+
+        public static string ToXml<T>(this DataTable dataTable) where T : new()
+        {
+            return DataSerializer.SerializeDataTableToXmlListOfType<T>(dataTable);
+        }
+    }
+}
+
---
+++ Extensions/ListExtensions.cs
@@ -0,0 +1,35 @@
+﻿using DataManagement.Tools;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace DataManagement.Extensions
+{
+    public static class ListExtensions
+    {
+        public static void ToExcel<T>(this List<T> list, string fullyQualifiedFileName)
+        {
+            ExcelSerializer.SerializeListOfTypeToExcel(list, fullyQualifiedFileName);
+        }
+
+        public static List<T> FromExcel<T>(this List<T> list, string worksheetName, string fullyQualifiedFileName) where T : new()
+        {
+            return ExcelSerializer.DeserializeExcelToListOfType<T>(worksheetName, fullyQualifiedFileName);
+        }
+
+        public static void ToFile<T>(this List<T> list, string fullyQualifiedFileName, char separator)
+        {
+            FileSerializer.SerializeListOfTypeToFile(list, fullyQualifiedFileName, separator);
+        }
+
+        public static List<T> FromFile<T>(this List<T> list, string fullyQualifiedFileName, char separator, Encoding encoding) where T : new()
+        {
+            return FileSerializer.DeserializeFileToListOfType<T>(fullyQualifiedFileName, separator, encoding);
+        }
+
+        public static DataTable ToDataTable<T>(this List<T> list) where T : new()
+        {
+            return DataSerializer.ConvertListToDataTableOfType<T>(list);
+        }
+    }
+}
