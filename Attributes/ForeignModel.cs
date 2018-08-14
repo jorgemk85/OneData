655dc63609da19566f48e45ab1e372d0d00a02a4
Jorge Marcos Kawas <jorgemk85@hotmail.com>
2018-08-13 20:49:39 -0500

    Correccion del archivo ForeignTable a ForeignModel.
    Solo se aceptan tipos que implementen IManageable en ForeignModel.

---
+++ Attributes/ForeignModel.cs
@@ -0,0 +1,23 @@
+﻿using DataManagement.Interfaces;
+using System;
+
+namespace DataManagement.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ForeignModel : Attribute
+    {
+        public Type Model { get; set; }
+
+        public ForeignModel(Type model)
+        {
+            if (model.IsAssignableFrom(typeof(IManageable)))
+            {
+                Model = model;
+            }
+            else
+            {
+                throw new NotSupportedException(string.Format("El tipo '{0}' no es aceptable debido a que no implementa la interfaz '{1}'.", nameof(model), nameof(IManageable)));
+            }
+        }
+    }
+}
