8aef077c99da08558b1dc28e97bd03eea0b5acd4
Jorge Marcos Kawas <jorgemk85@hotmail.com>
2019-05-19 02:15:16 -0500

    Initial aproach to the new query system. Still testing alternatives.

---
+++ Extensions/QueryBuilder/InnerJoinExtensions.cs
@@ -0,0 +1,23 @@
+﻿using OneData.Models.QueryBuilder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneData.Extensions.QueryBuilder
+{
+    public static class InnerJoinExtensions
+    {
+        public static WhereClause<T> Where<T>(this InnerJoinKeyword innerJoin, params Expression<Func<T, bool>>[] conditions)
+        {
+            return new WhereClause<T>();
+        }
+
+        public static InnerJoinKeyword InnerJoin<T, J>(this InnerJoinKeyword selectStatement, Expression<Func<T, dynamic>> id)
+        {
+            return new InnerJoinKeyword();
+        }
+    }
+}
